1b1b81857e6f72a4369c50f09bc4c21d3143d409
Lauren Malone <lauren.malone@devtegrate.com>
2023-01-26 14:08:01 -0600

    second commit for this

--- app.js
+++ app.js
@@ -2,9 +2,29 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 3000; 
 
+app.set('view engine', 'ejs');
+
+let myName = 'Lauren';
+
 
 app.get('/', (req, res) => {
-  res.send('Hello World, this is november')
+//   res.send('Hello World, this is november');
+  res.send(`<h3> Hi, ${ myName }</h3>`);
+})
+
+app.get('/show', (req, res) => {
+    // res.sendFile('index.html');
+    res.sendFile('index.html' , { root : __dirname});
+
+})
+
+app.get('/ejs', (req, res) => {
+    // ejs stuff here
+    // use res.render to load up an ejs view file
+
+        res.render('index');
+    
+
 })
 
 console.log('in the node console');
